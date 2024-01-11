48c744f3ce2bd53f320133ec1f302ba157f0c43b
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2024-01-10 20:10:31 -0500

    debug fix

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-const createDebug = require('debug');
 const config = require('./config/config');
 const bot = require('./lib/bot');
 const { startActions, userActions, adminActions } = require('./actions');
 require('./config/db');
-
-const debug = createDebug('bot');
 
 startActions();
 userActions();
 adminActions();
 
 const production = () => {
-  debug('Bot runs in production mode');
-  debug(`${config.username} setting webhook: ${config.webHook}`);
+  console.log('Bot runs in production mode');
+  console.log(`${config.username} setting webhook: ${config.webHook}`);
   bot.telegram.setWebhook(config.webHook);
-  debug(`${config.username} starting webhook on port: ${config.port}`);
+  console.log(`${config.username} starting webhook on port: ${config.port}`);
 };
 
 const development = () => {
-  debug('Bot runs in development mode');
-  debug(`${config.username} deleting webhook`);
+  console.log('Bot runs in development mode');
+  console.log(`${config.username} deleting webhook`);
   bot.telegram.deleteWebhook();
-  debug(`${config.username} starting polling`);
+  console.log(`${config.username} starting polling`);
   bot.launch();
 };
 
